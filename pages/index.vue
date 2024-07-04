566e0ac21e9f1210ace1b87fa11b70447d0cc8bb
Kyle Tong <kyle.simon.tong@gmail.com>
2024-07-04 22:51:24 +0900

    WIP - spacing

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,12 @@
 <template>
-  <div class="h-full w-full">
-    <div class="h-screen w-9/10">
-      <picture src="~/assets/img/background-image.webp">
+  <div class="h-screen w-full">
+    <div class="h-full w-full flex justify-center">
+      <picture class="w-11/12" src="~/assets/img/background-image.webp">
         <img
           src="~/assets/img/background-image.webp"
           alt="Entrance design default picture of a pot plant on a rainy day"
         />
       </picture>
-      test
     </div>
   </div>
 </template>
