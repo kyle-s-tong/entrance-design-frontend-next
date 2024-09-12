f7bf347e0a48c543435cb175198d281f3079da9e
Kyle Tong <kyle.simon.tong@gmail.com>
2024-09-12 22:29:05 +0900

    Get strapi and tailwind working

---
+++ src/interfaces/pricings.ts
@@ -0,0 +1,10 @@
+export default interface Pricing {
+  id: number;
+  Title: string;
+  description: string;
+  content: string;
+  slug: string;
+  createdAt: string;
+  updatedAt: string;
+  publishedAt: string;
+}
